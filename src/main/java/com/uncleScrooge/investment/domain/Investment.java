f515742368a0ea183201e96b4e212494418b86e3
Eder Rodrigues <ederpr@gmail.com>
2024-08-27 06:31:22 -0300

    Classes de dominio de investimentos

---
+++ src/main/java/com/uncleScrooge/investment/domain/Investment.java
@@ -0,0 +1,9 @@
+package com.uncleScrooge.investment.domain;
+
+public class Investment {
+    private Integer id;
+    private String name;
+    private InvestmentType type;
+    private Double value;
+    private String broker;
+}
---
+++ src/test/java/com/uncleScrooge/investment/domain/InvestmentTest.java
@@ -0,0 +1,17 @@
+package com.uncleScrooge.investment.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvestmentTest {
+
+    private Investment investment;
+
+    @Test
+    public void should_return_erro_when_value_of_investment_is_equal_or_lower_then_zero(){
+        Exception e = assertThrows(IllegalArgumentException.class, () -> investment = new Investment());
+        assertEquals("Value equal or lower then zero not supported", e.getMessage());
+    }
+
+}
