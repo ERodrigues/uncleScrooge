ebc0203d231f8b748f020951ecc6702a07eded3e
Eder Rodrigues <ederpr@gmail.com>
2024-08-27 06:36:50 -0300

    Criacao do construtor e do metodo de validacao dos atributos

--- src/main/java/com/uncleScrooge/investment/domain/Investment.java
+++ src/main/java/com/uncleScrooge/investment/domain/Investment.java
@@ -6,4 +6,17 @@ public class Investment {
     private InvestmentType type;
     private Double value;
     private String broker;
+
+    public Investment(String name, InvestmentType type, double value, String broker) {
+        this.name = name;
+        this.type = type;
+        this.value = value;
+        this.broker = broker;
+        validate();
+    }
+
+    private void validate() {
+        if (this.value <= 0)
+            throw new IllegalArgumentException("Value equal or lower then zero not supported");
+    }
 }
